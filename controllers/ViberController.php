f904ced457410aa2476723236e5ac7f1b720f54e
eldorianwin@gmail.com <S@2NsH%T0MWIF3t9>
2020-03-30 16:38:00 +0300

    add viber bot

--- controllers/ViberController.php
+++ controllers/ViberController.php
@@ -49,195 +49,56 @@ class ViberController extends Controller
         try {
             $bot = new Bot(['token' => $apiKey]);
             $bot
-    // first interaction with bot - return "welcome message"
-    ->onConversation(function ($event) use ($bot, $botSender) {
-        return (new Text())
-            ->setSender($botSender)
-            ->setText("Добрый день. Я бот РДЦ. Выберите, что вы хотите сделать")
-            ->setKeyboard(
-                (new Keyboard())
-                    ->setButtons([
-                        (new Button())
-                            ->setBgColor('#2fa4e7')
-                            ->setTextHAlign('center')
-                            ->setActionType('reply')
-                            ->setActionBody('btn-click')
-                            ->setText('Получить заключение'),
-                        (new Button())
-                            ->setBgColor('#2fa4e7')
-                            ->setTextHAlign('center')
-                            ->setActionType('reply')
-                            ->setActionBody('btn-click')
-                            ->setText('Что-то ещё'),
-
-                    ])
-            );
-    })
-    // when user subscribe to PA
-    ->onSubscribe(function ($event) use ($bot, $botSender) {
-        $this->getClient()->sendMessage(
-            (new Text())
-                ->setSender($botSender)
-                ->setText('Сейчас продолжим')
-        );
-    })
-    ->onText('|btn-click|s', function ($event) use ($bot, $botSender) {
-        $receiverId = $event->getSender()->getId();
-        $bot->getClient()->sendMessage(
-            (new Text())
-                ->setSender($botSender)
-                ->setReceiver($receiverId)
-                ->setText('you press the button')
-        );
-    })
-    ->onText('|k\d+|is', function ($event) use ($bot, $botSender) {
-        $caseNumber = (int)preg_replace('|[^0-9]|s', '', $event->getMessage()->getText());
-        $client = $bot->getClient();
-        $receiverId = $event->getSender()->getId();
-        switch ($caseNumber) {
-            case 0:
-                $client->sendMessage(
-                    (new Text())
+                // first interaction with bot - return "welcome message"
+                ->onConversation(function ($event) use ($bot, $botSender) {
+                    return (new Text())
                         ->setSender($botSender)
-                        ->setReceiver($receiverId)
-                        ->setText('Basic keyboard layout')
+                        ->setText('Добрый день. Я бот РДЦ. Выберите, что вы хотите сделать')
                         ->setKeyboard(
                             (new Keyboard())
                                 ->setButtons([
-                                    (new Button())
-                                        ->setActionType('reply')
-                                        ->setActionBody('btn-click')
-                                        ->setText('Tap this button')
-                                ])
-                        )
-                );
-                break;
-            //
-            case 1:
-                $client->sendMessage(
-                    (new Text())
-                        ->setSender($botSender)
-                        ->setReceiver($receiverId)
-                        ->setText('More buttons and styles')
-                        ->setKeyboard(
-                            (new Keyboard())
-                                ->setButtons([
-                                    (new Button())
-                                        ->setBgColor('#8074d6')
-                                        ->setTextSize('small')
-                                        ->setTextHAlign('right')
-                                        ->setActionType('reply')
-                                        ->setActionBody('btn-click')
-                                        ->setText('Button 1'),
-
                                     (new Button())
                                         ->setBgColor('#2fa4e7')
                                         ->setTextHAlign('center')
                                         ->setActionType('reply')
-                                        ->setActionBody('btn-click')
-                                        ->setText('Button 2'),
-
+                                        ->setActionBody('get-conclusion')
+                                        ->setText('Получить заключение'),
                                     (new Button())
-                                        ->setBgColor('#555555')
-                                        ->setTextSize('large')
-                                        ->setTextHAlign('left')
+                                        ->setBgColor('#2fa4e7')
+                                        ->setTextHAlign('center')
                                         ->setActionType('reply')
-                                        ->setActionBody('btn-click')
-                                        ->setText('Button 3'),
-                                ])
-                        )
-                );
-                break;
-            //
-            case 2:
-                $client->sendMessage(
-                    (new \Viber\Api\Message\Contact())
-                        ->setSender($botSender)
-                        ->setReceiver($receiverId)
-                        ->setName('Novikov Bogdan')
-                        ->setPhoneNumber('+380000000000')
-                );
-                break;
-            //
-            case 3:
-                $client->sendMessage(
-                    (new \Viber\Api\Message\Location())
-                        ->setSender($botSender)
-                        ->setReceiver($receiverId)
-                        ->setLat(48.486504)
-                        ->setLng(35.038910)
-                );
-                break;
-            //
-            case 4:
-                $client->sendMessage(
-                    (new \Viber\Api\Message\Sticker())
-                        ->setSender($botSender)
-                        ->setReceiver($receiverId)
-                        ->setStickerId(114408)
-                );
-                break;
-            //
-            case 5:
-                $client->sendMessage(
-                    (new \Viber\Api\Message\Url())
-                        ->setSender($botSender)
-                        ->setReceiver($receiverId)
-                        ->setMedia('https://hcbogdan.com')
-                );
-                break;
-            //
-            case 6:
-                $client->sendMessage(
-                    (new \Viber\Api\Message\Picture())
-                        ->setSender($botSender)
-                        ->setReceiver($receiverId)
-                        ->setText('some media data')
-                        ->setMedia('https://developers.viber.com/img/devlogo.png')
-                );
-                break;
-            //
-            case 7:
-                $client->sendMessage(
-                    (new \Viber\Api\Message\Video())
-                        ->setSender($botSender)
-                        ->setReceiver($receiverId)
-                        ->setSize(2 * 1024 * 1024)
-                        ->setMedia('http://techslides.com/demos/sample-videos/small.mp4')
-                );
-                break;
-            //
-            case 8:
-                $client->sendMessage(
-                    (new \Viber\Api\Message\CarouselContent())
-                        ->setSender($botSender)
-                        ->setReceiver($receiverId)
-                        ->setButtonsGroupColumns(6)
-                        ->setButtonsGroupRows(6)
-                        ->setBgColor('#FFFFFF')
-                        ->setButtons([
-                            (new Button())
-                                ->setColumns(6)
-                                ->setRows(3)
-                                ->setActionType('open-url')
-                                ->setActionBody('https://www.google.com')
-                                ->setImage('https://i.vimeocdn.com/portrait/58832_300x300'),
+                                        ->setActionBody('more-actions')
+                                        ->setText('Что-то ещё'),
 
-                            (new Button())
-                                ->setColumns(6)
-                                ->setRows(3)
-                                ->setActionType('reply')
-                                ->setActionBody('https://www.google.com')
-                                ->setText('<span style="color: #ffffff; ">Buy</span>')
-                                ->setTextSize("large")
-                                ->setTextVAlign("middle")
-                                ->setTextHAlign("middle")
-                                ->setImage('https://s14.postimg.org/4mmt4rw1t/Button.png')
-                        ])
-                );
-                break;
-        }
-    })
+                                ])
+                        );
+                })
+                // when user subscribe to PA
+                ->onSubscribe(function ($event) use ($bot, $botSender) {
+                    $bot->getClient()->sendMessage(
+                        (new Text())
+                            ->setSender($botSender)
+                            ->setText('Сейчас продолжим')
+                    );
+                })
+                ->onText('|get-conclusion|s', function ($event) use ($bot, $botSender) {
+                    $receiverId = $event->getSender()->getId();
+                    $bot->getClient()->sendMessage(
+                        (new Text())
+                            ->setSender($botSender)
+                            ->setReceiver($receiverId)
+                            ->setText('Введите номер обследования')
+                    );
+                })
+                ->onText('|more-actions|s', function ($event) use ($bot, $botSender) {
+                    $receiverId = $event->getSender()->getId();
+                    $bot->getClient()->sendMessage(
+                        (new Text())
+                            ->setSender($botSender)
+                            ->setReceiver($receiverId)
+                            ->setText('Напишите, что бы вы хотели сделать')
+                    );
+                })
                 ->run();
         } catch (Exception $e) {
             // todo - log exceptions
