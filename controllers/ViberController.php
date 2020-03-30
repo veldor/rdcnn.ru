d5050d1c81200f1701a8d95271b22ff49a91f2b0
eldorianwin@gmail.com <S@2NsH%T0MWIF3t9>
2020-03-30 14:52:39 +0300

    add viber bot

--- controllers/ViberController.php
+++ controllers/ViberController.php
@@ -53,7 +53,7 @@ class ViberController extends Controller
                         ->setSender($botSender)
                         ->setText('Чё как, сучара?');
                 })
-                ->onText('.+', function ($event) use ($bot, $botSender) {
+                ->onText('|whois .*|si', function ($event) use ($bot, $botSender) {
                     // это событие будет вызвано если пользователь пошлет сообщение
                     // которое совпадет с регулярным выражением
                     $bot->getClient()->sendMessage(
