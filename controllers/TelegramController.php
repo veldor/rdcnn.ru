c16929d8d5002cfa16057893d8c9804f51772683
eldorianwin@gmail.com <S@2NsH%T0MWIF3t9>
2020-03-30 09:34:56 +0300

    add telegram bot

--- controllers/TelegramController.php
+++ controllers/TelegramController.php
@@ -5,32 +5,45 @@ namespace app\controllers;
 
 
 use app\priv\Info;
+use Exception;
 use TelegramBot\Api\Client;
 use TelegramBot\Api\InvalidJsonException;
 use yii\web\Controller;
 
 class TelegramController extends Controller
 {
-    /**
-     * @throws InvalidJsonException
-     */
     public function actionConnect(): void
     {
-        $token = Info::TG_BOT_TOKEN;
-        $bot = new Client($token);
+        try{
+            $token = Info::TG_BOT_TOKEN;
+            $bot = new Client($token);
 // команда для start
-        $bot->command('start', static function ($message) use ($bot) {
-            $answer = 'Добро пожаловать!';
-            $bot->sendMessage($message->getChat()->getId(), $answer);
-        });
+            $bot->command('start', static function ($message) use ($bot) {
+                $answer = 'Добро пожаловать!';
+                $bot->sendMessage($message->getChat()->getId(), $answer);
+            });
 
 // команда для помощи
-        $bot->command('help', function ($message) use ($bot) {
-            $answer = 'Команды:
+            $bot->command('help', function ($message) use ($bot) {
+                $answer = 'Команды:
 /help - вывод справки';
-            $bot->sendMessage($message->getChat()->getId(), $answer);
-        });
+                $bot->sendMessage($message->getChat()->getId(), $answer);
+            });
 
-        $bot->run();
+            try {
+                $bot->run();
+            } catch (InvalidJsonException $e) {
+                // запишу ошибку в лог
+                $file = dirname($_SERVER['DOCUMENT_ROOT'] . './/') . '/logs/telebot_' . time() . '.log';
+                $report = $e->getMessage();
+                file_put_contents($file, $report);
+            }
+        }
+        catch (Exception $e){
+            // запишу ошибку в лог
+            $file = dirname($_SERVER['DOCUMENT_ROOT'] . './/') . '/logs/telebot_err_' . time() . '.log';
+            $report = $e->getMessage();
+            file_put_contents($file, $report);
+        }
     }
 }
