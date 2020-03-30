2a10874234812c610df80494fcccecf2db5c2fbe
eldorianwin@gmail.com <S@2NsH%T0MWIF3t9>
2020-03-30 09:23:31 +0300

    add telegram bot

--- controllers/TelegramController.php
+++ controllers/TelegramController.php
@@ -4,12 +4,33 @@
 namespace app\controllers;
 
 
+use app\priv\Info;
+use TelegramBot\Api\Client;
+use TelegramBot\Api\InvalidJsonException;
 use yii\web\Controller;
 
 class TelegramController extends Controller
 {
+    /**
+     * @throws InvalidJsonException
+     */
     public function actionConnect(): void
     {
-        echo 'connected';
+        $token = Info::TG_BOT_TOKEN;
+        $bot = new Client($token);
+// команда для start
+        $bot->command('start', static function ($message) use ($bot) {
+            $answer = 'Добро пожаловать!';
+            $bot->sendMessage($message->getChat()->getId(), $answer);
+        });
+
+// команда для помощи
+        $bot->command('help', function ($message) use ($bot) {
+            $answer = 'Команды:
+/help - вывод справки';
+            $bot->sendMessage($message->getChat()->getId(), $answer);
+        });
+
+        $bot->run();
     }
 }
