c50967b72f1f5159a4f02cb4835425c8e346db95
eldorianwin@gmail.com <S@2NsH%T0MWIF3t9>
2020-06-04 22:56:24 +0300

    bug fixes

--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,8 @@
 <?php
 
 // comment out the following two lines when deployed to production
+use yii\base\InvalidConfigException;
+
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
@@ -9,4 +11,7 @@ require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../config/web.php';
 
-(new yii\web\Application($config))->run();
+try {
+    (new yii\web\Application($config))->run();
+} catch (InvalidConfigException $e) {
+}
