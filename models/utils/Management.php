5e12d4c4bc2439cc77f3bbc1bd63faaaa485e87d
veldor <eldorianwin@gmail.com>
2020-07-02 22:04:15 +0300

    work with telegram bot

--- models/utils/Management.php
+++ models/utils/Management.php
@@ -35,7 +35,7 @@ class Management extends Model
         }
         else{
                 // запишу в файл отчётов, что ещё не пришло время для проверки
-                return 'timeout ' . (300 - (time() - FileUtils::getLastUpdateTime()));
+                return 'timeout ' . (60 - (time() - FileUtils::getLastUpdateTime()));
         }
         return false;
     }
