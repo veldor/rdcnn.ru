3929c670a41e929c87fa4c0b75026b4913a61119
eldor <eldorianwin@gmail.com>
2021-01-03 11:16:55 +0300

    start API

--- models/PersonalApi.php
+++ models/PersonalApi.php
@@ -20,6 +20,7 @@ class PersonalApi
     public static function handleRequest(): array
     {
         $data = json_decode(file_get_contents('php://input'), true);
+        Telegram::sendDebug("Доступ к API: " . $data);
         return $data;
     }
 }
