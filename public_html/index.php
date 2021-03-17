bce0a36d051afb5068832cb3366bc7f976754e2b
eldor <eldorianwin@gmail.com>
2021-03-17 13:21:46 +0300

    start API

--- public_html/index.php
+++ public_html/index.php
@@ -3,8 +3,11 @@
 // comment out the following two lines when deployed to production
 use app\models\utils\MyErrorHandler;
 
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+$host = $_SERVER['REMOTE_ADDR'];
+if($host === '127.0.0.1'){
+    defined('YII_DEBUG') or define('YII_DEBUG', true);
+    defined('YII_ENV') or define('YII_ENV', 'dev');
+}
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
