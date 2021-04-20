cb0d44991206f60f71853918538604308e4cf73d
eldor <eldorianwin@gmail.com>
2021-04-20 08:33:24 +0300

    start API

--- models/Utils.php
+++ models/Utils.php
@@ -190,6 +190,8 @@ class Utils extends Model
         /** @var PatientInfo $patient */
         foreach (PatientInfoDouble::find()->each(100) as $patient) {
             // проверю, что адреса ещё нет в базе
+            var_dump(PatientInfo::find()->where(['email' => $patient->email])->count());
+            die;
             if(PatientInfo::find()->where(['email' => $patient->email])->count() === 0){
                 $new = new PatientInfo();
                 $new->email = $patient->email;
